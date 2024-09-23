ea9be9554c7b7972f0570ff8038f6435a9e3b57a
xcurvnubaim <zhariffmarzuqi@gmail.com>
2024-09-23 07:29:09 +0700

    feat: add dark theme for table item

--- resources/views/livewire/items/items.blade.php
+++ resources/views/livewire/items/items.blade.php
@@ -16,20 +16,27 @@
 
     <table class="table-fixed w-full">
         <thead>
-            <tr class="bg-gray-200">
-                <th class="px-4 py-2">Item Name</th>
-                <th class="px-4 py-2">Item Stok</th>
-                <th class="px-4 py-2">Item Price</th>
-                <th class="px-4 py-2">Action</th>
+            <tr class="bg-gray-200 dark:bg-gray-700 dark:text-white">
+                <th class="dark:border-gray-500 w-16 px-4 py-2">No</th>
+                <th class="dark:border-gray-500 px-4 py-2">Item Name</th>
+                <th class="dark:border-gray-500 px-4 py-2">Item Stok</th>
+                <th class="dark:border-gray-500 px-4 py-2">Item Price</th>
+                <th class="dark:border-gray-500 px-4 py-2">Action</th>
             </tr>
         </thead>
         <tbody>
-            @foreach($items as $item)
+            @foreach($items as $index => $item)
             <tr>
-                <td class="border px-4 py-2">{{ $item->name }}</td>
-                <td class="border px-4 py-2">{{ $item->stock }}</td>
-                <td class="border px-4 py-2">{{ $item->price }}</td>
-                <td class="border px-4 py-2">
+                <td class="border dark:border-gray-500 text-center px-4 py-2 dark:text-white">{{ $index + 1 }}</td>
+                <td class="border dark:border-gray-500 text-start px-4 py-2 dark:text-white">{{ $item->name }}</td>
+                <td class="border dark:border-gray-500 text-center px-4 py-2 dark:text-white">{{ $item->stock }}</td>
+                <td class="border dark:border-gray-500 px-4 py-2 dark:text-white">
+                    <div class="flex justify-between">
+                        <span>Rp</span>
+                        <span>{{ number_format($item->price, 0, ',', '.') }}</span>
+                    </div>
+                </td>
+                <td class="border dark:border-gray-500 text-center py-2 gap-4 flex justify-center">
                     <button wire:click="edit({{ $item->id }})" class="btn bg-blue-600 text-white px-3 py-2 rounded-md">Edit</button>
                     <button wire:click="delete({{ $item->id }})" class="btn bg-red-600 text-white px-3 py-2 rounded-md">Delete</button>
                 </td>
